d82597495bbfc0b1f3c9da1061e39fb21b3f4a2b
nonentityboy <1138580559@qq.com>
2020-01-30 01:31:22 +0800

    add ejs

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/:name', function (req, res) {
-  res.send('hello, ' + req.params.name)
+  res.render('users', {
+    name: req.params.name
+  })
 })
 
 module.exports = router
